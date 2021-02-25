1e18f62198ffec3d0e90ee85847f9fce29eb2b61
amirbavand <amirhossein.bavand@gmail.com>
2021-02-24 19:44:17 -0500

    some changes to authentication

---
+++ auth/src/routes/signout.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const router=express.Router();
+
+router.get('/api/users/signout', (req, res)=>{
+
+    res.send('hiii');
+
+
+})
+
+export {router as signoutRouter}
